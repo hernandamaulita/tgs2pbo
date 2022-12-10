f05ce38dc7cd4ef12f51a66d1f41057ff554bf1c
hernandamaulita <101704028+hernandamaulita@users.noreply.github.com>
2022-12-10 19:49:52 +0700

    Add files via upload

---
+++ Kereta.java
@@ -0,0 +1,13 @@
+public abstract class Kereta {
+    String KA;
+
+    void setKA(String KA) {
+        this.KA = KA;
+    }
+
+    String getKA() {
+        return KA;
+    }
+
+    abstract int getArea();
+}
